825e9f7cee4a32adfd1987843ee051a9c4c02042
augustojose17 <melarajose83@gmail.com>
2024-06-25 09:07:39 -0600

    Add files via upload

--- Tienda Virtual/scripts.js
+++ Tienda Virtual/scripts.js
@@ -149,61 +149,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Al cargar la página, asegúrate de mostrar los productos del carrito si hay alguno guardado
     updateCart();
+
+    // Agregar evento para el botón de "Realizar Compra"
+    const realizarCompraButton = document.querySelector('.realizar-compra');
+    realizarCompraButton.addEventListener('click', vaciarCarrito);
+
+    function vaciarCarrito() {
+        // Vaciar el array del carrito
+        cart = [];
+        
+        // Remover el carrito del localStorage
+        localStorage.removeItem('cart');
+        
+        // Actualizar la visualización del carrito
+        updateCart();
+
+        // Mostrar mensaje de confirmación (opcional)
+        alert('Compra realizada con éxito. El carrito ha sido vaciado.');
+    }
 });
-(function(){
-    const listElements = document.querySelectorAll('.menu__item--show');
-    const list = document.querySelector('.menu__links');
-    const menu = document.querySelector('.menu__hamburguer');
-
-    const addClick = ()=>{
-        listElements.forEach(element =>{
-            element.addEventListener('click', ()=>{
-
-                
-                let subMenu = element.children[1];
-                let height = 0;
-                element.classList.toggle('menu__item--active');
-
-
-                if(subMenu.clientHeight === 0){
-                    height = subMenu.scrollHeight;
-                }
-
-                subMenu.style.height = `${height}px`;
-
-            });
-        });
-    }
-
-    const deleteStyleHeight = ()=>{
-        listElements.forEach(element=>{
-
-            if(element.children[1].getAttribute('style')){
-                element.children[1].removeAttribute('style');
-                element.classList.remove('menu__item--active');
-            }
-
-        });
-    }
-
-
-    window.addEventListener('resize', ()=>{
-        if(window.innerWidth > 800){
-            deleteStyleHeight();
-            if(list.classList.contains('menu__links--show'))
-                list.classList.remove('menu__links--show');
-
-        }else{
-            addClick();
-        }
-    });
-
-    if(window.innerWidth <= 800){
-        addClick();
-    }
-
-    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
-
-
-
-})();
